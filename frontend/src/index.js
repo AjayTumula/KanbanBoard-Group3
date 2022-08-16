a1f5549d05dbd85e5b31ad023a2f0fca1d9fa6bd
AjayTumula <ajaytumula72@gmail.com>
2022-08-16 21:33:28 +0100

    added register-login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,18 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import KanbanBoard from "./components/KanbanBoard";
-
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { AuthProvider } from './context/AuthProvider';
+import './index.css';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<>Home</>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
         <Route path="/kanban_board" element={<KanbanBoard/>} />
       </Routes>
     </BrowserRouter>
@@ -15,4 +20,4 @@ export default function App() {
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<AuthProvider><App /></AuthProvider>);
